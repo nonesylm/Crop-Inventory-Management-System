f999c8712d4ce6fe56a7bc12b7d227c39632b9b0
nonesylm <nonescan.shielamae26@gmail.com>
2024-11-28 10:37:04 +0800

    Update BulkOrderProcessor.java

--- src/finalprojecta/BulkOrderProcessor.java
+++ src/finalprojecta/BulkOrderProcessor.java
@@ -1,10 +1,27 @@
 package finalprojecta;
 
 public class BulkOrderProcessor extends OrderProcessor {
+  private InventoryManagement inventorymanagement; 
+
+  public BulkOrderProcessor(InventoryManagement inventorymanagement) {
+    this.inventoryManagement = inventoryManagement; 
+  }
   
     @Override
     public void processOrder(Order order) {
-       
+      System.out.println("Processing bulk order for + order.getCropName() + "..."); 
+      
+        if (inventoryManagement.isAvailable(order.getCropName(), order.getQuantity())) {
+            inventoryManagement.deductStock(order.getCropName(), order.getQuantity());
+            order.setStatus("Processed (Bulk)");
+
+            System.out.println("Bulk order processed successfully for " + order.getCropName() +
+                    ". Quantity: " + order.getQuantity());
+
+      } else {
+            order.setStatus("Failed (Insufficient Stock)");
+            System.out.println("Bulk order failed for " + order.getCropName() +
+                    ". Insufficient stock available.");
+        }
     }
 }
-
