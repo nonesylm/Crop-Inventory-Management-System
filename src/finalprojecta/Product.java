75c829b873d120896041ac75ee1463a3b153a2e8
nonesylm <nonescan.shielamae26@gmail.com>
2024-11-28 10:25:15 +0800

    Update Product.java

--- src/finalprojecta/Product.java
+++ src/finalprojecta/Product.java
@@ -4,13 +4,12 @@ public abstract class Product {
     private String name;
     private int quantity;
 
-    // Constructor
     public Product(String name, int quantity) {
         this.name = name;
         this.quantity = quantity;
     }
 
-    // Getter and Setter methods
+
     public String getName() {
         return name;
     }
